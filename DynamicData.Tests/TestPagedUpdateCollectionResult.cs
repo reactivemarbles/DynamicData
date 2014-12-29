eab8a323771ba864d8522bbed0d21f96d3bdeb74
Roland Pheasant <roland_pheasant@hotmail.com>
2014-12-29 14:11:14 +0000

    Tidy up

--- DynamicData.Tests/TestPagedUpdateCollectionResult.cs
+++ DynamicData.Tests/TestPagedUpdateCollectionResult.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using DynamicData.Diagnostics;
-using DynamicData.Kernel;
 
 namespace DynamicData.Tests
 {
     public class TestPagedChangeSetResult<TObject, TKey> : IDisposable
     {
-        private readonly IList<IPagedChangeSet<TObject, TKey>> _messages = new List<IPagedChangeSet<TObject, TKey>>();
-        private ChangeSummary _summary;
-        private Exception _error;
-        private readonly IDisposable _disposer;
-
         private readonly IObservableCache<TObject, TKey> _data;
+        private readonly IDisposable _disposer;
+        private readonly IList<IPagedChangeSet<TObject, TKey>> _messages = new List<IPagedChangeSet<TObject, TKey>>();
+        private Exception _error;
+        private ChangeSummary _summary;
 
         public TestPagedChangeSetResult(IObservable<IPagedChangeSet<TObject, TKey>> source)
         {
@@ -22,20 +20,19 @@ namespace DynamicData.Tests
 
             var error = published.Subscribe(updates => { }, ex => _error = ex);
             var results = published.Subscribe(updates => _messages.Add(updates));
-
             _data = published.AsObservableCache();
             var summariser = published.CollectUpdateStats().Subscribe(summary => _summary = summary);
 
             var connected = published.Connect();
-            _disposer = Disposable.Create(() =>
-                                              {
-                                                  connected.Dispose();
-                                                  summariser.Dispose();
-                                                  results.Dispose();
-                                                  error.Dispose();
-                                              });
-        }
 
+            _disposer = Disposable.Create(() =>
+            {
+                connected.Dispose();
+                summariser.Dispose();
+                results.Dispose();
+                error.Dispose();
+            });
+        }
 
 
         public IObservableCache<TObject, TKey> Data
