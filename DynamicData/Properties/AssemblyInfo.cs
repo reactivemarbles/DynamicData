44ac9a2e60b367bde0f3bba740305671c618cb47
Roland Pheasant <roland_pheasant@hotmail.com>
2015-01-03 14:06:08 +0000

    Scrap legacy portability

--- DynamicData/Properties/AssemblyInfo.cs
+++ DynamicData/Properties/AssemblyInfo.cs
@@ -16,10 +16,7 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("DynamicData.Tests")]
 [assembly: InternalsVisibleTo("DynamicData.PLinq")]
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+
 
 
 
