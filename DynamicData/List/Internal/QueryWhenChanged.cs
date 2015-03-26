d85632bfad0299ad1a36989a3d09ef4c40d397a8
Roland Pheasant <roland_pheasant@hotmail.com>
2015-03-26 11:18:43 +0000

    Missed file

---
+++ DynamicData/List/Internal/QueryWhenChanged.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using DynamicData.Annotations;
+
+namespace DynamicData.Internal
+{
+	internal class QueryWhenChanged<T>
+	{
+		private readonly IObservable<IChangeSet<T>> _source;
+		readonly IList<T> _list = new List<T>();
+
+		public QueryWhenChanged([NotNull] IObservable<IChangeSet<T>> source)
+		{
+			if (source == null) throw new ArgumentNullException("source");
+			_source = source;
+		}
+
+
+		public IObservable<IList<T>> Run()
+		{
+			return _source.Do(_list.Clone).Select(_=>new ReadOnlyCollection<T>(_list));
+		}
+	}
+}
