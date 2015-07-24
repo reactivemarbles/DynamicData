3da3eed2ae3d33e00da08c119304540bf18e7cd2
Roland Pheasant <roland_pheasant@hotmail.com>
2015-07-24 19:08:06 +0100

    Avg() operator

---
+++ DynamicData/Aggregation/Avg.cs
@@ -0,0 +1,14 @@
+namespace DynamicData.Aggregation
+{
+    internal struct Avg<TValue>
+    {
+        public Avg(int count, TValue sum)
+        {
+            Count = count;
+            Sum = sum;
+        }
+
+        public int Count { get; }
+        public TValue Sum { get; }
+    }
+}
