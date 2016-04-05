05281160642c0325a852ace1118e0d7acead9113
Roland Pheasant <roland@dynamic-data.org>
2016-04-05 19:42:49 +0100

    Create observable change set from readonly observable collection

--- DynamicData/Binding/ObservableCollectionEx.cs
+++ DynamicData/Binding/ObservableCollectionEx.cs
@@ -168,7 +168,86 @@ namespace DynamicData.Binding
                         var subscriber = cloneOfList.Connect().SubscribeSafe(observer);
                         return new CompositeDisposable(cacheLoader, subscriber, cloneOfList);
                     }
+                );
+        }
 
+
+        /// <summary>
+        /// Convert an observable collection into a dynamic stream of change sets
+        /// </summary>
+        /// <typeparam name="TObject">The type of the object.</typeparam>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="keySelector">The key selector.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">source
+        /// or
+        /// keySelector</exception>
+        public static IObservable<IChangeSet<TObject, TKey>> ToObservableChangeSet<TObject, TKey>(this ReadOnlyObservableCollection<TObject> source, Func<TObject, TKey> keySelector)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+
+            return Observable.Create<IChangeSet<TObject, TKey>>
+                (
+                    observer =>
+                    {
+                        //populate local cache, otherwise there is no way to deal with a reset
+                        var cloneOfList = new SourceCache<TObject, TKey>(keySelector);
+
+                        Func<ChangeSet<TObject, TKey>> initialChangeSet = () =>
+                        {
+                            var items = source.Select(t => new Change<TObject, TKey>(ChangeReason.Add, keySelector(t), t));
+                            return new ChangeSet<TObject, TKey>(items);
+                        };
+
+                        var sourceUpdates = Observable.FromEventPattern<NotifyCollectionChangedEventHandler, NotifyCollectionChangedEventArgs>(
+                                           h => ((INotifyCollectionChanged)source).CollectionChanged += h,
+                                           h => ((INotifyCollectionChanged)source).CollectionChanged -= h)
+                           .Select
+                            (
+                                args =>
+                                {
+                                    var changes = args.EventArgs;
+
+                                    switch (changes.Action)
+                                    {
+                                        case NotifyCollectionChangedAction.Add:
+                                            return changes.NewItems.OfType<TObject>()
+                                                .Select(t => new Change<TObject, TKey>(ChangeReason.Add, keySelector(t), t));
+
+                                        case NotifyCollectionChangedAction.Remove:
+                                            return changes.OldItems.OfType<TObject>()
+                                                .Select(t => new Change<TObject, TKey>(ChangeReason.Remove, keySelector(t), t));
+
+                                        case NotifyCollectionChangedAction.Replace:
+                                            {
+                                                return changes.NewItems.OfType<TObject>()
+                                                    .Select((t, idx) =>
+                                                    {
+                                                        var old = changes.OldItems[idx];
+                                                        return new Change<TObject, TKey>(ChangeReason.Update, keySelector(t), t, (TObject)old);
+                                                    });
+                                            }
+                                        case NotifyCollectionChangedAction.Reset:
+                                            {
+                                                //Clear all from the cache and reload
+                                                var removes = cloneOfList.KeyValues.Select(t => new Change<TObject, TKey>(ChangeReason.Remove, t.Key, t.Value)).ToArray();
+                                                return removes.Concat(initialChangeSet());
+                                            }
+                                        default:
+                                            return null;
+                                    }
+                                })
+                            .Where(updates => updates != null)
+                            .Select(updates => (IChangeSet<TObject, TKey>)new ChangeSet<TObject, TKey>(updates));
+
+
+                        var initialChanges = initialChangeSet();
+                        var cacheLoader = Observable.Return(initialChanges).Concat(sourceUpdates).PopulateInto(cloneOfList);
+                        var subscriber = cloneOfList.Connect().SubscribeSafe(observer);
+                        return new CompositeDisposable(cacheLoader, subscriber, cloneOfList);
+                    }
                 );
         }
     }
