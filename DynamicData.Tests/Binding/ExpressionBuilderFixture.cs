987bb4f7fc8ba0b5d16a9c099779d1ada9e66394
Roland Pheasant <roland@dynamic-data.org>
2016-12-01 20:10:03 +0000

    Start code to notify property changes on a chain of properties

---
+++ DynamicData.Tests/Binding/ExpressionBuilderFixture.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynamicData.Binding;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace DynamicData.Tests.Binding
+{
+    [TestFixture]
+    public class ExpressionBuilderFixture
+    {
+        [Test]
+        public void GenerateExpression()
+        {
+            var instance =new ClassA();
+            
+            var members1 = ExpressionBuilder.GetMembers<ClassA, string>(a => a.Name).ToArray();
+
+            var members2 = ExpressionBuilder.GetMembers<ClassA, int>(a => a.Child.Age).ToArray();
+
+            var xxxx = members2
+                .Select(m => m.GetProperty())
+                .ToArray();
+
+            
+
+        }
+
+        public class ClassA: AbstractNotifyPropertyChanged
+        {
+            private string _name;
+
+            public string Name
+            {
+                get { return _name; }
+                set { SetAndRaise(ref _name, value); }
+            }
+
+            private ClassB _classB;
+
+            public ClassB Child
+            {
+                get { return _classB; }
+                set { SetAndRaise(ref _classB, value); }
+            }
+        }
+
+        public class ClassB : AbstractNotifyPropertyChanged
+        {
+            private int _age;
+
+            public int Age
+            {
+                get { return _age; }
+                set { SetAndRaise(ref _age, value); }
+            }
+        }
+
+    }
+}
--- DynamicData/Cache/Internal/FilterOnProperty.cs
+++ DynamicData/Cache/Internal/FilterOnProperty.cs
@@ -3,58 +3,10 @@ using System.ComponentModel;
 using System.Linq.Expressions;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using DynamicData.Annotations;
 using DynamicData.Internal;
 
 namespace DynamicData.Cache.Internal
 {
-    internal class QueryWhenChanged<TObject, TKey, TValue>
-    {
-        private readonly IObservable<IChangeSet<TObject, TKey>> _source;
-        private readonly Func<TObject, IObservable<TValue>> _itemChangedTrigger;
-
-        public QueryWhenChanged([NotNull] IObservable<IChangeSet<TObject, TKey>> source,
-                    [NotNull] Func<TObject, IObservable<TValue>> itemChangedTrigger = null)
-        {
-            if (source == null) throw new ArgumentNullException(nameof(source));
-            _source = source;
-            _itemChangedTrigger = itemChangedTrigger;
-        }
-
-        public IObservable<IQuery<TObject, TKey>> Run()
-        {
-            return Observable.Create<IQuery<TObject, TKey>>(observer =>
-            {
-                var locker = new object();
-                var cache = new Cache<TObject, TKey>();
-                var query = new AnonymousQuery<TObject, TKey>(cache);
-
-                if (_itemChangedTrigger != null)
-                {
-                    return _source.Publish(shared =>
-                    {
-                        var inlineChange = shared.MergeMany(_itemChangedTrigger)
-                            .Synchronize(locker)
-                            .Select(_ => query);
-
-                        var sourceChanged = shared
-                            .Synchronize(locker)
-                            .Do(changes => cache.Clone(changes))
-                            .Select(changes => query);
-
-                        return sourceChanged.Merge(inlineChange);
-                    }).SubscribeSafe(observer);
-                }
-                else
-                {
-                    return _source.Do(changes => cache.Clone(changes))
-                        .Select(changes => query)
-                        .SubscribeSafe(observer);
-                }
-            });
-        }
-    }
-
     internal class FilterOnProperty<TObject, TKey, TProperty>
         where TObject : INotifyPropertyChanged
     {
