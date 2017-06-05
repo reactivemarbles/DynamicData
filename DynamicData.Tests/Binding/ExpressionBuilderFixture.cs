1af5fcaac7664a5bd0c6387bc39a96ead019534e
Roland Pheasant <roland@dynamic-data.org>
2017-06-05 21:30:02 +0100

    Deeply nested property notifications

--- DynamicData.Tests/Binding/ExpressionBuilderFixture.cs
+++ DynamicData.Tests/Binding/ExpressionBuilderFixture.cs
@@ -1,61 +1,240 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
 using DynamicData.Binding;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace DynamicData.Tests.Binding
-{
+{              
+
     [TestFixture]
     public class ExpressionBuilderFixture
     {
         [Test]
-        public void GenerateExpression()
+        public void NotifiesInitialValue()
         {
-            var instance =new ClassA();
-            
-            var members1 = ExpressionBuilder.GetMembers<ClassA, string>(a => a.Name).ToArray();
-            var members2 = ExpressionBuilder.GetMembers<ClassA, int>(a => a.Child.Age).ToArray();
+            var instance = new ClassA {Child = new ClassB {Age = 10}};
 
-            var xxxx = members2
-                .Select(m => m.GetProperty())
-                .ToArray();
+            var chain =  instance.ObserveChain(a => a.Child.Age, true);
+            int? result = null;
 
-            //require the following
-            //1) Lambda to get value.
-            //2) Check property changes for each parent object i.e. is a.Child INPC
+            var subscription = chain.Subscribe(notification => result = notification?.Value);
 
-           // xxxx.Select(p => p.MemberType)
+            result.Should().Be(10);
 
+            instance.Child.Age = 22;
+            result.Should().Be(22);
+
+            instance.Child = new ClassB {Age = 25};
+            result.Should().Be(25);
+
+            instance.Child.Age = 26;
+            result.Should().Be(26);
+            instance.Child = null;
+            result.Should().Be(null);
+
+            instance.Child = new ClassB { Age = 21 };
+            result.Should().Be(21);
         }
 
-        public class ClassA: AbstractNotifyPropertyChanged
+        [Test]
+        public void NotifiesInitialValueAndNullChild()
+        {
+            var instance = new ClassA();
+
+            var chain = instance.ObserveChain(a => a.Child.Age, true);
+            int? result = null;
+
+            var subscription = chain.Subscribe(notification => result = notification?.Value);
+            result.Should().Be(null);
+            instance.Child = new ClassB { Age = 10 };
+
+            result.Should().Be(10);
+
+            instance.Child.Age = 22;
+            result.Should().Be(22);
+
+            instance.Child = new ClassB { Age = 25 };
+            result.Should().Be(25);
+
+            instance.Child.Age = 26;
+            result.Should().Be(26);
+            instance.Child = null;
+         
+        }
+
+        [Test]
+        public void WithoutInitialValue()
+        {
+            var instance = new ClassA {Name="TestClass", Child = new ClassB {Age = 10}};
+
+            var chain = instance.ObserveChain(a => a.Child.Age, false);
+            int? result = null;
+
+            var subscription = chain.Subscribe(notification => result = notification.Value);
+
+            result.Should().Be(null);
+
+            instance.Child.Age = 22;
+            result.Should().Be(22);
+
+            instance.Child = new ClassB {Age = 25};
+            result.Should().Be(25);
+
+            instance.Child.Age = 30;
+            result.Should().Be(30);
+        }
+
+        [Test]
+        public void NullChildWithoutInitialValue()
+        {
+            var instance = new ClassA();
+
+            var chain = instance.ObserveChain(a => a.Child.Age, false);
+            int? result = null;
+
+            var subscription = chain.Subscribe(notification => result = notification.Value);
+
+            result.Should().Be(null);
+
+            instance.Child = new ClassB { Age = 21 };
+            result.Should().Be(21);
+
+            instance.Child.Age = 22;
+            result.Should().Be(22);
+
+            instance.Child = new ClassB { Age = 25 };
+            result.Should().Be(25);
+
+            instance.Child.Age = 30;
+            result.Should().Be(30);
+        }
+
+        public class ClassA: AbstractNotifyPropertyChanged, IEquatable<ClassA>
         {
             private string _name;
 
             public string Name
             {
-                get { return _name; }
-                set { SetAndRaise(ref _name, value); }
+                get => _name;
+                set => SetAndRaise(ref _name, value);
             }
 
             private ClassB _classB;
 
             public ClassB Child
             {
-                get { return _classB; }
-                set { SetAndRaise(ref _classB, value); }
+                get => _classB;
+                set => SetAndRaise(ref _classB, value);
+            }
+
+            #region Equality
+
+            public bool Equals(ClassA other)
+            {
+                if (ReferenceEquals(null, other)) return false;
+                if (ReferenceEquals(this, other)) return true;
+                return string.Equals(_name, other._name) && Equals(_classB, other._classB);
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((ClassA) obj);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return ((_name != null ? _name.GetHashCode() : 0) * 397) ^ (_classB != null ? _classB.GetHashCode() : 0);
+                }
+            }
+
+            /// <summary>Returns a value that indicates whether the values of two <see cref="T:DynamicData.Tests.Binding.ExpressionBuilderFixture.ClassA" /> objects are equal.</summary>
+            /// <param name="left">The first value to compare.</param>
+            /// <param name="right">The second value to compare.</param>
+            /// <returns>true if the <paramref name="left" /> and <paramref name="right" /> parameters have the same value; otherwise, false.</returns>
+            public static bool operator ==(ClassA left, ClassA right)
+            {
+                return Equals(left, right);
+            }
+
+            /// <summary>Returns a value that indicates whether two <see cref="T:DynamicData.Tests.Binding.ExpressionBuilderFixture.ClassA" /> objects have different values.</summary>
+            /// <param name="left">The first value to compare.</param>
+            /// <param name="right">The second value to compare.</param>
+            /// <returns>true if <paramref name="left" /> and <paramref name="right" /> are not equal; otherwise, false.</returns>
+            public static bool operator !=(ClassA left, ClassA right)
+            {
+                return !Equals(left, right);
+            }
+
+            #endregion
+
+            public override string ToString()
+            {
+                return $"ClassA: Name={Name}, {nameof(Child)}: {Child}";
             }
         }
 
-        public class ClassB : AbstractNotifyPropertyChanged
+
+        public class ClassB : AbstractNotifyPropertyChanged, IEquatable<ClassB>
         {
             private int _age;
 
             public int Age
             {
-                get { return _age; }
-                set { SetAndRaise(ref _age, value); }
+                get => _age;
+                set => SetAndRaise(ref _age, value);
+            }
+
+            #region Equality
+
+            public bool Equals(ClassB other)
+            {
+                if (ReferenceEquals(null, other)) return false;
+                if (ReferenceEquals(this, other)) return true;
+                return _age == other._age;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((ClassB) obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return _age;
+            }
+
+            /// <summary>Returns a value that indicates whether the values of two <see cref="T:DynamicData.Tests.Binding.ExpressionBuilderFixture.ClassB" /> objects are equal.</summary>
+            /// <param name="left">The first value to compare.</param>
+            /// <param name="right">The second value to compare.</param>
+            /// <returns>true if the <paramref name="left" /> and <paramref name="right" /> parameters have the same value; otherwise, false.</returns>
+            public static bool operator ==(ClassB left, ClassB right)
+            {
+                return Equals(left, right);
+            }
+
+            /// <summary>Returns a value that indicates whether two <see cref="T:DynamicData.Tests.Binding.ExpressionBuilderFixture.ClassB" /> objects have different values.</summary>
+            /// <param name="left">The first value to compare.</param>
+            /// <param name="right">The second value to compare.</param>
+            /// <returns>true if <paramref name="left" /> and <paramref name="right" /> are not equal; otherwise, false.</returns>
+            public static bool operator !=(ClassB left, ClassB right)
+            {
+                return !Equals(left, right);
+            }
+
+            #endregion
+
+            public override string ToString()
+            {
+                return $"{nameof(Age)}: {Age}";
             }
         }
 
--- DynamicData/Binding/ExpressionBuilder.cs
+++ DynamicData/Binding/ExpressionBuilder.cs
@@ -1,12 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Reflection;
+
 
 namespace DynamicData.Binding
 {
     internal static class ExpressionBuilder
     {
+        internal static IObservable<PropertyValue<TObject, TProperty>> ObserveChain<TObject, TProperty>(this TObject source, Expression<Func<TObject, TProperty>> expression, bool notifyInitial = true)
+            where TObject : INotifyPropertyChanged
+        {
+            var chain = source.CreatePropertyChain(expression, notifyInitial);
+            return chain.CreateObservable();
+        }
+
+        private static PropertyChain<TObject, TProperty> CreatePropertyChain<TObject, TProperty>(this TObject source, Expression<Func<TObject, TProperty>> expression, bool notifyInitial = true)
+            where TObject : INotifyPropertyChanged
+        {
+            MemberExpression outer = expression.Body as MemberExpression; 
+            MemberExpression[] members = GetMembers(expression).ToArray();
+
+            var chain = expression.GetMemberChain().ToArray();
+
+
+            var propertyChangedMonitor = chain.Select(m => new PropertyChainPart(m)).ToArray();
+            return new PropertyChain<TObject, TProperty>(source, expression, propertyChangedMonitor, notifyInitial);
+        }
+
+        private static IEnumerable<MemberExpression> GetMemberChain<TObject, TProperty>(this Expression<Func<TObject, TProperty>> expression)
+        {
+            var memberExpression = expression.Body as MemberExpression;
+            while (memberExpression != null)
+            {
+                if (memberExpression.Expression.NodeType != ExpressionType.Parameter)
+                {
+                    var parent = memberExpression.Expression;
+                    yield return memberExpression.Update(Expression.Parameter(parent.Type));
+                }
+                else
+                {
+                    yield return memberExpression;
+                }
+                memberExpression = memberExpression.Expression as MemberExpression;
+            }
+        }
+        //Require: Need a value accessor to get the value from the parent
+
+        internal static Func<object, object> CreateValueAccessor(this MemberExpression source)
+        {
+            //create an expression which accepts the parent and returns the child
+            var property = source.GetProperty();
+            var method = property.GetMethod;
+           
+            //convert the parameter i.e. the declaring class to an object
+            var parameter = Expression.Parameter(typeof(object));
+            var converted = Expression.Convert(parameter, source.Expression.Type);
+
+            //call the get value of the property and box it
+            var propertyCall = Expression.Call(converted, method);
+            var boxed = Expression.Convert(propertyCall, typeof(object));
+            var accessorExpr = Expression.Lambda<Func<object, object>>(boxed, parameter);
+
+            var accessor = accessorExpr.Compile();
+            return accessor;
+        }
+
+        internal static Func<object, IObservable<Unit>> CreatePropertyChangedFactory(this MemberExpression source)
+        {
+           
+            var property = source.GetProperty();
+            var inpc = typeof(INotifyPropertyChanged).GetTypeInfo().IsAssignableFrom(property.DeclaringType.GetTypeInfo());
+
+            return t =>
+            {
+                if (t == null) return Observable.Never<Unit>();
+                if (!inpc) return Observable.Return(Unit.Default);
+
+                return Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>
+                    (
+                        handler => ((INotifyPropertyChanged) t).PropertyChanged += handler,
+                        handler => ((INotifyPropertyChanged) t).PropertyChanged -= handler
+                    )
+                    .Where(args => args.EventArgs.PropertyName == property.Name)
+                    .Select(args => Unit.Default);
+            };
+        }
+
+        internal static Func<object, IObservable<PropertyValue<object,object>>> CreatePropertyValueChangedFactory(this MemberExpression source)
+        {
+            var property = source.GetProperty();
+          
+            var inpc = typeof(INotifyPropertyChanged).GetTypeInfo().IsAssignableFrom(property.DeclaringType.GetTypeInfo());
+            var valueAccessor = CreateValueAccessor(source);
+           
+            return t =>
+            {   
+                if (t == null) return Observable.Never<PropertyValue<object, object>>();
+
+
+
+                if (!inpc) return Observable.Return(new PropertyValue<object, object>(t, valueAccessor(t)));
+
+                return Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>
+                    (
+                        handler => ((INotifyPropertyChanged)t).PropertyChanged += handler,
+                        handler => ((INotifyPropertyChanged)t).PropertyChanged -= handler
+                    )
+                    .Where(args => args.EventArgs.PropertyName == property.Name)
+                    .Select(args => new PropertyValue<object, object>(args.Sender, valueAccessor(args.Sender)));
+            };
+        }
 
         public static IEnumerable<MemberExpression> GetMembers<TObject, TProperty>(Expression<Func<TObject, TProperty>> expression)
         {
