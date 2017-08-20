8caab9a4b1f8dd316c1e59c799e52a95af3b3f6f
Roland Pheasant <roland@dynamic-data.org>
2017-08-20 13:59:01 +0100

    Get tests working for v4

---
+++ DynamicData.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("DynamicData.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DynamicData.Tests")]
+[assembly: AssemblyCopyright("Copyright \u00A9 Roland Pheasant 2011-2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3565fbfd-1ceb-4189-ab02-27e86fb9213d")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
