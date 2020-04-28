533d41aa0a80c54f53254caffc63d0c519295d5d
Erlend Angelsen <erlend.angelsen@gmail.com>
2020-04-28 09:33:09 +0200

    Add IsEmpty / NotEmpty overloads to CountEx (#250)

--- src/DynamicData/Aggregation/CountEx.cs
+++ src/DynamicData/Aggregation/CountEx.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for full license information.
 
 using System;
+using System.Reactive.Linq;
 
 namespace DynamicData.Aggregation
 {
@@ -56,6 +57,68 @@ namespace DynamicData.Aggregation
         public static IObservable<int> Count<TObject>(this IObservable<IDistinctChangeSet<TObject>> source)
         {
             return source.ForAggregation().Count();
+        }
+
+        /// <summary>
+        /// Counts the total number of items in the underlying data source
+        /// and return true if the number of items == 0
+        /// </summary>
+        /// <typeparam name="TObject">The type of the object.</typeparam>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <returns></returns>
+        public static IObservable<bool> IsEmpty<TObject, TKey>(this IObservable<IChangeSet<TObject, TKey>> source)
+        {
+            return source.ForAggregation()
+                .Count()
+                .StartWith(0)
+                .Select(count => count == 0);
+        }
+
+        /// <summary>
+        /// Counts the total number of items in the underlying data source
+        /// and returns true if the number of items is greater than 0
+        /// </summary>
+        /// <typeparam name="TObject">The type of the object.</typeparam>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <returns></returns>
+        public static IObservable<bool> NotEmpty<TObject, TKey>(this IObservable<IChangeSet<TObject, TKey>> source)
+        {
+            return source.ForAggregation()
+                .Count()
+                .StartWith(0)
+                .Select(count => count > 0);
+        }
+
+        /// <summary>
+        /// Counts the total number of items in the underlying data source
+        /// and return true if the number of items == 0
+        /// </summary>
+        /// <typeparam name="TObject">The type of the object.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <returns></returns>
+        public static IObservable<bool> IsEmpty<TObject>(this IObservable<IChangeSet<TObject>> source)
+        {
+            return source.ForAggregation()
+                .Count()
+                .StartWith(0)
+                .Select(count => count == 0);
+        }
+
+        /// <summary>
+        /// Counts the total number of items in the underlying data source
+        /// and returns true if the number of items is greater than 0
+        /// </summary>
+        /// <typeparam name="TObject">The type of the object.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <returns></returns>
+        public static IObservable<bool> NotEmpty<TObject>(this IObservable<IChangeSet<TObject>> source)
+        {
+            return source.ForAggregation()
+                .Count()
+                .StartWith(0)
+                .Select(count => count > 0);
         }
     }
 }
