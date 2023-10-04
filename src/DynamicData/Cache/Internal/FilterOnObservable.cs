1005bb3c618097a767f5bd9e3c4abcff5ce43f71
Darrin W. Cullop <Darrin.Cullop@microsoft.com>
2023-10-03 18:02:24 -0700

    Use AutoRefreshOnObservable

--- src/DynamicData/Cache/Internal/FilterOnObservable.cs
+++ src/DynamicData/Cache/Internal/FilterOnObservable.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for full license information.
 
 using System.Reactive.Concurrency;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
 
 namespace DynamicData.Cache.Internal;
@@ -15,48 +14,23 @@ internal class FilterOnObservable<TObject, TKey>
     private readonly Func<TObject, TKey, IObservable<bool>> _filterFactory;
     private readonly IObservable<IChangeSet<TObject, TKey>> _source;
     private readonly TimeSpan? _buffer;
-    private readonly IScheduler _scheduler;
+    private readonly IScheduler? _scheduler;
 
     public FilterOnObservable(IObservable<IChangeSet<TObject, TKey>> source, Func<TObject, TKey, IObservable<bool>> filterFactory, TimeSpan? buffer = null, IScheduler? scheduler = null)
     {
         _source = source ?? throw new ArgumentNullException(nameof(source));
         _filterFactory = filterFactory ?? throw new ArgumentNullException(nameof(filterFactory));
         _buffer = buffer;
-        _scheduler = scheduler ?? Scheduler.Default;
+        _scheduler = scheduler;
     }
 
-    public IObservable<IChangeSet<TObject, TKey>> Run() =>
-        Observable.Create<IChangeSet<TObject, TKey>>(observer =>
-        {
-            var shared = _source.Transform(val => new FilterProxy(val)).Publish();
-
-            // For each proxy object, create the filter observable, and use it to update the Filter property
-            // Create a Refresh Change each time it is updated.
-            // Merge the streams of Refresh Changes together.
-            var changes =
-                shared.MergeMany((proxy, key) =>
-                        proxy.UpdateFilter(_filterFactory(proxy.Value, key))
-                             .Select(_ => new Change<FilterProxy, TKey>(ChangeReason.Refresh, key, proxy)));
-
-            // Create a stream of changesets from the stream of changes (either one at a time or buffered)
-            IObservable<IChangeSet<FilterProxy, TKey>> refreshChanges = _buffer is null
-                ? changes.Select(c => new ChangeSet<FilterProxy, TKey>(new[] { c }))
-                : changes.Buffer(_buffer.Value, _scheduler)
-                         .Where(list => list.Count > 0)
-                         .Select(items => new ChangeSet<FilterProxy, TKey>(items));
-
-            // Merge the Refresh Changes and the regular changes together
-            // Filter for objects passing the filter
-            // Transform back to the original object type
-            var locker = new object();
-            var publisher = shared.Synchronize(locker)
-                                            .Merge(refreshChanges.Synchronize(locker))
-                                            .Filter(proxy => proxy.PassesFilter)
-                                            .Transform(proxy => proxy.Value)
-                                            .SubscribeSafe(observer);
-
-            return new CompositeDisposable(publisher, shared.Connect());
-        });
+    public IObservable<IChangeSet<TObject, TKey>> Run()
+    {
+        return _source.Transform(val => new FilterProxy(val))
+                        .AutoRefreshOnObservable((proxy, key) => proxy.UpdateFilter(_filterFactory(proxy.Value, key)), _buffer, _scheduler)
+                        .Filter(proxy => proxy.PassesFilter)
+                        .Transform(proxy => proxy.Value);
+    }
 
     private class FilterProxy
     {
@@ -70,6 +44,6 @@ internal class FilterOnObservable<TObject, TKey>
         public bool PassesFilter { get; private set; }
 
         public IObservable<bool> UpdateFilter(IObservable<bool> observable) =>
-            observable.DistinctUntilChanged().Select(filterValue => PassesFilter = filterValue);
+            observable.DistinctUntilChanged().Do(filterValue => PassesFilter = filterValue);
     }
 }
