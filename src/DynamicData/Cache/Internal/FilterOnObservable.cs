fe9d058c66191d38fe7718531cbe9fb569b8851b
Darrin W. Cullop <Darrin.Cullop@microsoft.com>
2023-10-05 16:08:24 -0700

    Added Unit Tests for FilterOnObservable

--- src/DynamicData/Cache/Internal/FilterOnObservable.cs
+++ src/DynamicData/Cache/Internal/FilterOnObservable.cs
@@ -26,24 +26,24 @@ internal class FilterOnObservable<TObject, TKey>
 
     public IObservable<IChangeSet<TObject, TKey>> Run()
     {
-        return _source.Transform(val => new FilterProxy(val))
-                        .AutoRefreshOnObservable((proxy, key) => proxy.UpdateFilter(_filterFactory(proxy.Value, key)), _buffer, _scheduler)
-                        .Filter(proxy => proxy.PassesFilter)
-                        .Transform(proxy => proxy.Value);
+        return _source.Transform((val, key) => new FilterProxy(val, _filterFactory(val, key)))
+                      .AutoRefreshOnObservable(proxy => proxy.FilterObservable, _buffer, _scheduler)
+                      .Filter(proxy => proxy.PassesFilter)
+                      .Transform(proxy => proxy.Value);
     }
 
     private class FilterProxy
     {
-        public FilterProxy(TObject obj)
+        public FilterProxy(TObject obj, IObservable<bool> observable)
         {
             Value = obj;
+            FilterObservable = observable.DistinctUntilChanged().Do(filterValue => PassesFilter = filterValue);
         }
+
+        public IObservable<bool> FilterObservable { get; }
 
         public TObject Value { get; }
 
         public bool PassesFilter { get; private set; }
-
-        public IObservable<bool> UpdateFilter(IObservable<bool> observable) =>
-            observable.DistinctUntilChanged().Do(filterValue => PassesFilter = filterValue);
     }
 }
