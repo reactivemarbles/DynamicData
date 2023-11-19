99ca55173d55a3eff0720cfcae41e8e6d73ed4af
Chris Pulman <chris.pulman@yahoo.com>
2023-11-19 19:40:55 +0000

    Revert back to original code after locating the issue with non awaited code

---
+++ src/DynamicData.Tests/API/ApiExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using System.Threading.Tasks;
+using PublicApiGenerator;
+using VerifyXunit;
+using Xunit;
+
+namespace DynamicData.APITests;
+
+/// <summary>
+/// A helper for doing API approvals.
+/// </summary>
+[ExcludeFromCodeCoverage]
+[UsesVerify]
+public static class ApiExtensions
+{
+    /// <summary>
+    /// Checks to make sure the API is approved.
+    /// </summary>
+    /// <param name="assembly">The assembly that is being checked.</param>
+    /// <param name="namespaces">The namespaces.</param>
+    /// <param name="filePath">The caller file path.</param>
+    /// <returns>
+    /// A Task.
+    /// </returns>
+    public static async Task CheckApproval(this Assembly assembly, string[] namespaces, [CallerFilePath] string filePath = "")
+    {
+        var generatorOptions = new ApiGeneratorOptions { AllowNamespacePrefixes = namespaces };
+        var apiText = assembly.GeneratePublicApi(generatorOptions);
+        var result = await Verifier.Verify(apiText, null, filePath)
+            .UniqueForRuntimeAndVersion()
+            .ScrubEmptyLines()
+            .ScrubLines(l =>
+                l.StartsWith("[assembly: AssemblyVersion(", StringComparison.InvariantCulture) ||
+                l.StartsWith("[assembly: AssemblyFileVersion(", StringComparison.InvariantCulture) ||
+                l.StartsWith("[assembly: AssemblyInformationalVersion(", StringComparison.InvariantCulture) ||
+                l.StartsWith("[assembly: System.Reflection.AssemblyMetadata(", StringComparison.InvariantCulture));
+    }
+}
