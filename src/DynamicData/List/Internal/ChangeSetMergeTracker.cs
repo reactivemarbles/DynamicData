0d33faac2119215b80a44e498b37879d31b55fb5
Darrin W. Cullop <Darrin.Cullop@microsoft.com>
2023-12-02 14:12:44 -0800

    First drop of List to List MergeManyChangeSets

---
+++ src/DynamicData/List/Internal/ChangeSetMergeTracker.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) 2011-2023 Roland Pheasant. All rights reserved.
+// Roland Pheasant licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace DynamicData.List.Internal;
+
+internal class ChangeSetMergeTracker<TObject>
+    where TObject : notnull
+{
+    private readonly ChangeAwareList<TObject> _resultList = new();
+
+    public void ProcessChangeSet(IChangeSet<TObject> changes, IObserver<IChangeSet<TObject>> observer)
+    {
+        foreach (var change in changes)
+        {
+            switch (change.Reason)
+            {
+                case ListChangeReason.Add:
+                    OnItemAdded(change.Item);
+                    break;
+
+                case ListChangeReason.Remove:
+                    OnItemRemoved(change.Item);
+                    break;
+
+                case ListChangeReason.Replace:
+                    OnItemReplaced(change.Item);
+                    break;
+
+                case ListChangeReason.Refresh:
+                    OnItemRefreshed(change.Item);
+                    break;
+
+                case ListChangeReason.AddRange:
+                    OnRangeAdded(change.Range);
+                    break;
+
+                case ListChangeReason.RemoveRange:
+                    OnRangeRemoved(change.Range);
+                    break;
+
+                case ListChangeReason.Clear:
+                    OnClear(change);
+                    break;
+            }
+        }
+
+        EmitChanges(observer);
+    }
+
+    public void RemoveItems(IList<TObject> sourceList, IObserver<IChangeSet<TObject>> observer)
+    {
+        _resultList.Remove(sourceList);
+        EmitChanges(observer);
+    }
+
+    private void OnClear(Change<TObject> change) => _resultList.ClearOrRemoveMany(change);
+
+    private void OnItemAdded(ItemChange<TObject> item) => _resultList.Add(item.Current);
+
+    private void OnItemRefreshed(ItemChange<TObject> item) => _resultList.Refresh(item.Current);
+
+    private void OnItemRemoved(ItemChange<TObject> item) => _resultList.Remove(item.Current);
+
+    private void OnItemReplaced(ItemChange<TObject> item) => _resultList.ReplaceOrAdd(item.Previous.Value, item.Current);
+
+    private void OnRangeAdded(RangeChange<TObject> range) => _resultList.AddRange(range);
+
+    private void OnRangeRemoved(RangeChange<TObject> range) => _resultList.Remove(range);
+
+    private void EmitChanges(IObserver<IChangeSet<TObject>> observer)
+    {
+        var changeSet = _resultList.CaptureChanges();
+        if (changeSet.Count != 0)
+        {
+            observer.OnNext(changeSet);
+        }
+    }
+
+}
---
+++ src/DynamicData/List/Internal/MergeManyListChangeSets.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) 2011-2023 Roland Pheasant. All rights reserved.
+// Roland Pheasant licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace DynamicData.List.Internal;
+
+internal class MergeManyListChangeSets<TObject, TDestination>(IObservable<IChangeSet<TObject>> source, Func<TObject, IObservable<IChangeSet<TDestination>>> selector)
+    where TObject : notnull
+    where TDestination : notnull
+{
+    public IObservable<IChangeSet<TDestination>> Run() => Observable.Create<IChangeSet<TDestination>>(
+            observer =>
+            {
+                var locker = new object();
+
+                // Transform to an observable list of cached lists
+                var sourceListofLists = source
+                                            .Transform(obj => new ChangeSetCache<TDestination>(selector(obj)))
+                                            .Synchronize(locker)
+                                            .AsObservableList();
+
+                var shared = sourceListofLists.Connect().Publish();
+
+                // This is manages all of the changes
+                var changeTracker = new ChangeSetMergeTracker<TDestination>();
+
+                // Berge the items back together
+                var allChanges = shared.MergeMany(mc => mc.Source.RemoveIndex())
+                                                 .Synchronize(locker)
+                                                 .Subscribe(
+                                                        changes => changeTracker.ProcessChangeSet(changes, observer),
+                                                        observer.OnError,
+                                                        observer.OnCompleted);
+
+                // When a source item is removed, all of its sub-items need to be removed
+                var removedItems = shared
+                    .OnItemRemoved(mc => changeTracker.RemoveItems(mc.List, observer))
+                    .Subscribe();
+
+                return new CompositeDisposable(sourceListofLists, allChanges, removedItems, shared.Connect());
+            });
+}
