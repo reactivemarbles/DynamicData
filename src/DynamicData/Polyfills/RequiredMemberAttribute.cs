70e02b5eb2322368b397f3b8bb587231789338bf
Jake Meiergerd <jakenveina@gmail.com>
2023-12-03 10:25:16 -0600

    Added "polyfills" to allow internal usage of some C#11 compiler features, including the `required` and `init` keywords. (#772)
    Co-authored-by: Chris Pulman <chris.pulman@yahoo.com>

---
+++ src/DynamicData/Polyfills/RequiredMemberAttribute.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2011-2023 Roland Pheasant. All rights reserved.
+// Roland Pheasant licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices;
+
+// Allows use of the C#11 `required` keyword, internally within this library, when targeting frameworks older than .NET 7.
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+internal sealed class RequiredMemberAttribute
+    : Attribute
+{
+}
