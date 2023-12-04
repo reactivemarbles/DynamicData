d0c6430e749c581a970ad50599924b31aa781b32
Roland Pheasant <roland_pheasant@hotmail.com>
2023-12-04 23:50:15 +0000

    Include index =0 for inital change set on source list. Fixes #723

---
+++ src/DynamicData.Tests/List/SourceListFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace DynamicData.Tests.List;
+
+public class SourceListFixture
+{
+    [Fact]
+    public void InitialChangeIsRange()
+    {
+        var source = new SourceList<string>();
+        source.Add("A");
+        var changeSets = new List<IChangeSet<string>>();
+
+        source.Connect().Subscribe(changeSets.Add).Dispose();
+
+
+        changeSets[0].First().Type.Should().Be(ChangeType.Range);
+        changeSets[0].First().Range.Index.Should().Be(0);
+    }
+}
