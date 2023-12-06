d960f582a6c582e7b475d6ed41d4cf90a0752429
JakenVeina <jakenveina@gmail.com>
2023-12-06 01:20:33 -0600

    Fixed broken setup for displaying Benchmark artifacts in Solution Explorer.

--- src/DynamicData.Benchmarks/Program.cs
+++ src/DynamicData.Benchmarks/Program.cs
@@ -2,12 +2,25 @@
 // Roland Pheasant licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
+using System.IO;
+using System.Runtime.CompilerServices;
+
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 
 namespace DynamicData.Benchmarks
 {
     public static class Program
     {
-        public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        public static void Main(string[] args)
+            => BenchmarkSwitcher
+                .FromAssembly(typeof(Program).Assembly)
+                .Run(args, DefaultConfig.Instance
+                    .WithArtifactsPath(Path.Combine(
+                        GetProjectRootDirectory(),
+                        Path.GetFileName(DefaultConfig.Instance.ArtifactsPath))));
+
+        private static string GetProjectRootDirectory([CallerFilePath] string? callerFilePath = null)
+            => Path.GetDirectoryName(callerFilePath)!;
     }
 }
