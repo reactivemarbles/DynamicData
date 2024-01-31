d6d748e2a76c9facf92f016a141078537e11d413
Jake Meiergerd <jakenveina@gmail.com>
2024-01-31 15:55:48 -0600

    Feature/stateless filtering and transforming (#823)
    * Fixed a build defect, present only for Debug builds, within Cache/TransformManyAsyncFixture
    
    * Adjusted .editorconfig to allow for multi-line expressions to be passed as method parameters.
    
    * Adjusted .editorconfig to re-allow omission of brackets for single-line code blocks.
    
    * Consolidated language settings across all projects, and added polyfills to .Benchmarks, to match the other two.
    
    * Added new operators `.FilterImmutable()` and `.TransformImmutable()` to provide better performance for static/deterministic scenarios.

---
+++ src/DynamicData/Cache/Internal/FilterImmutable.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (c) 2011-2023 Roland Pheasant. All rights reserved.
+// Roland Pheasant licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace DynamicData.Cache.Internal;
+
+internal sealed class FilterImmutable<TObject, TKey>
+    where TObject : notnull
+    where TKey : notnull
+{
+    private readonly Action<IObserver<IChangeSet<TObject, TKey>>, IChangeSet<TObject, TKey>> _onNextInvoker;
+    private readonly Func<TObject, bool> _predicate;
+    private readonly IObservable<IChangeSet<TObject, TKey>> _source;
+
+    public FilterImmutable(
+        Func<TObject, bool> predicate,
+        IObservable<IChangeSet<TObject, TKey>> source,
+        bool suppressEmptyChangeSets)
+    {
+        _predicate = predicate;
+        _source = source;
+
+        // Optimize away the if check, if possible, per-instance
+        _onNextInvoker = suppressEmptyChangeSets
+            ? (observer, changes) =>
+            {
+                if (changes.Count is not 0)
+                {
+                    observer.OnNext(changes);
+                }
+            }
+            : (observer, changes) => observer.OnNext(changes);
+    }
+
+    public IObservable<IChangeSet<TObject, TKey>> Run()
+        => Observable.Create<IChangeSet<TObject, TKey>>(observer => _source
+            .SubscribeSafe(Observer.Create<IChangeSet<TObject, TKey>>(
+                onNext: upstreamChanges =>
+                {
+                    var downstreamChanges = new ChangeSet<TObject, TKey>(capacity: upstreamChanges.Count);
+
+                    try
+                    {
+                        foreach (var change in upstreamChanges.ToConcreteType())
+                        {
+                            // Propagate changes as follows:
+                            //      Don't propagate moves at all, since we don't preserve indexes.
+                            //      For Updates, propagate...
+                            //          ...an Add if the new item matches the predicate, but the old one doesn't
+                            //          ...a Remove if the old item matches the predicate, but the new one doesn't
+                            //          ...an Update if both items match the predicate
+                            //          ...nothing if neither items match the predicate
+                            //      For all other changes, propagate only if the value matches the predicate
+                            if (change.Reason is ChangeReason.Moved)
+                                continue;
+
+                            ChangeReason? downstreamReason = change.Reason switch
+                            {
+                                ChangeReason.Update => (_predicate.Invoke(change.Previous.Value), _predicate.Invoke(change.Current)) switch
+                                {
+                                    (false, true) => ChangeReason.Add,
+                                    (true, false) => ChangeReason.Remove,
+                                    (true, true) => change.Reason,
+                                    _ => null
+                                },
+                                _ => _predicate.Invoke(change.Current)
+                                    ? change.Reason
+                                    : null
+                            };
+
+                            if (downstreamReason is ChangeReason reason)
+                            {
+                                // Do not propagate indexes, we can't guarantee them to be correct, because we aren't caching items.
+                                downstreamChanges.Add(new(
+                                    reason: reason,
+                                    key: change.Key,
+                                    current: change.Current,
+                                    previous: (reason is ChangeReason.Update)
+                                        ? change.Previous
+                                        : default));
+                            }
+                        }
+                    }
+
+                    // We're invoking "untrusted" consumer code, (I.E. _predicate), so catch and propagate any errors
+                    catch (Exception error)
+                    {
+                        observer.OnError(error);
+                    }
+
+                    _onNextInvoker.Invoke(observer, downstreamChanges);
+                },
+                onError: observer.OnError,
+                onCompleted: observer.OnCompleted)));
+}
---
+++ src/DynamicData/Cache/Internal/TransformImmutable.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) 2011-2023 Roland Pheasant. All rights reserved.
+// Roland Pheasant licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace DynamicData.Cache.Internal;
+
+internal sealed class TransformImmutable<TDestination, TSource, TKey>
+    where TDestination : notnull
+    where TSource : notnull
+    where TKey : notnull
+{
+    private readonly IObservable<IChangeSet<TSource, TKey>> _source;
+    private readonly Func<TSource, TDestination> _transformFactory;
+
+    public TransformImmutable(
+        IObservable<IChangeSet<TSource, TKey>> source,
+        Func<TSource, TDestination> transformFactory)
+    {
+        _source = source;
+        _transformFactory = transformFactory;
+    }
+
+    public IObservable<IChangeSet<TDestination, TKey>> Run()
+        => Observable.Create<IChangeSet<TDestination, TKey>>(observer => _source
+            .SubscribeSafe(Observer.Create<IChangeSet<TSource, TKey>>(
+                onNext: upstreamChanges =>
+                {
+                    var downstreamChanges = new ChangeSet<TDestination, TKey>(capacity: upstreamChanges.Count);
+
+                    try
+                    {
+                        foreach (var change in upstreamChanges.ToConcreteType())
+                        {
+                            downstreamChanges.Add(new(
+                                reason: change.Reason,
+                                key: change.Key,
+                                current: _transformFactory.Invoke(change.Current),
+                                previous: change.Previous.HasValue
+                                    ? _transformFactory.Invoke(change.Previous.Value)
+                                    : default,
+                                currentIndex: change.CurrentIndex,
+                                previousIndex: change.PreviousIndex));
+                        }
+                    }
+
+                    // We're invoking "untrusted" consumer code, (I.E. _transformFactory), so catch and propagate any errors
+                    catch (Exception error)
+                    {
+                        observer.OnError(error);
+                    }
+
+                    observer.OnNext(downstreamChanges);
+                },
+                onError: observer.OnError,
+                onCompleted: observer.OnCompleted)));
+}
