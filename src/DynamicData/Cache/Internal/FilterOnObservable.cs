fb864201e5ca52e605650a55c13b2d8dbf5172c5
Darrin W. Cullop <Darrin.Cullop@microsoft.com>
2024-02-05 11:00:19 -0800

    Feature:  Leverage TransformImmutable (#845)
    * Use TransformImmutable in MergeManyChangeSetsSourceCompare
    * Use TransformImmutable in FilterOnObservable
    * Use Primary Constructor for helper proxy class
    * Re-Enable extra unit tests now that they seem to be passing

--- src/DynamicData/Cache/Internal/FilterOnObservable.cs
+++ src/DynamicData/Cache/Internal/FilterOnObservable.cs
@@ -14,23 +14,19 @@ internal sealed class FilterOnObservable<TObject, TKey>(IObservable<IChangeSet<T
     private readonly Func<TObject, TKey, IObservable<bool>> _filterFactory = filterFactory ?? throw new ArgumentNullException(nameof(filterFactory));
     private readonly IObservable<IChangeSet<TObject, TKey>> _source = source ?? throw new ArgumentNullException(nameof(source));
 
-    public IObservable<IChangeSet<TObject, TKey>> Run() => _source.Transform((val, key) => new FilterProxy(val, _filterFactory(val, key)))
-                      .AutoRefreshOnObservable(proxy => proxy.FilterObservable, buffer, scheduler)
-                      .Filter(proxy => proxy.PassesFilter)
-                      .Transform(proxy => proxy.Value);
+    public IObservable<IChangeSet<TObject, TKey>> Run() =>
+        _source
+            .Transform((val, key) => new FilterProxy(val, _filterFactory(val, key)))
+            .AutoRefreshOnObservable(proxy => proxy.FilterObservable, buffer, scheduler)
+            .Filter(proxy => proxy.PassesFilter)
+            .TransformImmutable(proxy => proxy.Value);
 
-    private sealed class FilterProxy
+    private sealed class FilterProxy(TObject obj, IObservable<bool> observable)
     {
-        public FilterProxy(TObject obj, IObservable<bool> observable)
-        {
-            Value = obj;
-            FilterObservable = observable.DistinctUntilChanged().Do(filterValue => PassesFilter = filterValue);
-        }
-
-        public IObservable<bool> FilterObservable { get; }
-
-        public TObject Value { get; }
+        public TObject Value { get; } = obj;
 
         public bool PassesFilter { get; private set; }
+
+        public IObservable<bool> FilterObservable => observable.DistinctUntilChanged().Do(filterValue => PassesFilter = filterValue);
     }
 }
