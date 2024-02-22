1e0e11b73ef1daa6b6ead741bdd5ba80b4089f69
Darrin W. Cullop <Darrin.Cullop@microsoft.com>
2024-02-21 17:59:39 -0800

    Bug Fix:  Consolidate changes made to Group Caches into a single ChangeSet (#863)
    Fixes #851 by leveraging the code from #852 to suspend the notifications about changes to the group contents until all of the groups have been updated.

--- src/DynamicData.Tests/Cache/GroupOnObservableFixture.cs
+++ src/DynamicData.Tests/Cache/GroupOnObservableFixture.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Linq;
-using Bogus;
-using DynamicData.Tests.Domain;
-using DynamicData.Binding;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+using Bogus;
+using DynamicData.Binding;
+using DynamicData.Kernel;
+using DynamicData.Tests.Domain;
 using FluentAssertions;
 using Xunit;
 
 using Person = DynamicData.Tests.Domain.Person;
-using System.Threading.Tasks;
-using DynamicData.Kernel;
 
 namespace DynamicData.Tests.Cache;
 
@@ -50,6 +51,7 @@ public class GroupOnObservableFixture : IDisposable
         // Assert
         _results.Data.Count.Should().Be(InitialCount);
         _results.Messages.Count.Should().Be(1, "The child observables fire on subscription so everything should appear as a single changeset");
+        _groupResults.Groups.Items.ForEach(group => group.Messages.Count.Should().Be(1));
         VerifyGroupingResults();
     }
 
@@ -65,6 +67,7 @@ public class GroupOnObservableFixture : IDisposable
         // Assert
         _results.Data.Count.Should().Be(InitialCount + AddCount);
         _results.Messages.Count.Should().Be(2, "Initial Adds and then the subsequent Additions should each be a single message");
+        _groupResults.Groups.Items.ForEach(group => group.Messages.Count.Should().BeLessThanOrEqualTo(2));
         VerifyGroupingResults();
     }
 
@@ -80,6 +83,7 @@ public class GroupOnObservableFixture : IDisposable
         // Assert
         _results.Data.Count.Should().Be(InitialCount - RemoveCount);
         _results.Messages.Count.Should().Be(2, "1 for Adds and 1 for Removes");
+        _groupResults.Groups.Items.ForEach(group => group.Messages.Count.Should().BeLessThanOrEqualTo(2));
         VerifyGroupingResults();
     }
 
@@ -97,6 +101,7 @@ public class GroupOnObservableFixture : IDisposable
         // Assert
         _results.Data.Count.Should().Be(InitialCount, "Only replacements were made");
         _results.Messages.Count.Should().Be(2, "1 for Adds and 1 for Updates");
+        _groupResults.Groups.Items.ForEach(group => group.Messages.Count.Should().BeLessThanOrEqualTo(2));
         VerifyGroupingResults();
     }
 
@@ -115,6 +120,7 @@ public class GroupOnObservableFixture : IDisposable
         // Assert
         _cache.Items.Select(p => p.FavoriteColor).Distinct().Count().Should().Be(colorCount - 1);
         _results.Messages.Count.Should().Be(2, "1 for Adds and 1 for Removes");
+        _groupResults.Data.Count.Should().Be(colorCount - 1, "{0} colors were used and then all of the {1} were removed", colorCount, removeColor);
         _groupResults.Messages.Count.Should().Be(2, "1 for Adds and 1 for Removes");
         _groupResults.Summary.Overall.Adds.Should().Be(colorCount);
         _groupResults.Summary.Overall.Removes.Should().Be(1);
@@ -142,9 +148,11 @@ public class GroupOnObservableFixture : IDisposable
         // Assert
         _cache.Items.Select(p => p.FavoriteColor).Distinct().Count().Should().Be(colorCount);
         _results.Messages.Count.Should().Be(2, "1 for Adds and 1 for Other Added Value");
+        _groupResults.Data.Count.Should().Be(colorCount);
         _groupResults.Messages.Count.Should().Be(1, "Shouldn't be removed/re-added");
         _groupResults.Summary.Overall.Adds.Should().Be(colorCount);
         _groupResults.Summary.Overall.Removes.Should().Be(0);
+        _groupResults.Groups.Lookup(removeColor).Value.Data.Count.Should().Be(1, "All the {0} were removed and then 1 was added back", removeColor);
         VerifyGroupingResults();
     }
 
@@ -329,6 +337,9 @@ public class GroupOnObservableFixture : IDisposable
 
         // Check each group
         expectedGroupings.ForEach(grouping => grouping.Should().BeEquivalentTo(groupResults.Groups.Lookup(grouping.Key).Value.Data.Items));
+
+        // No groups should be empty
+        groupResults.Groups.Items.ForEach(group => group.Data.Count.Should().BeGreaterThan(0, "Empty groups should be removed"));
     }
 
     private static IObservable<Color> CreateFavoriteColorObservable(Person person, string key) =>
--- src/DynamicData/Cache/Internal/DynamicGrouper.cs
+++ src/DynamicData/Cache/Internal/DynamicGrouper.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for full license information.
 
 using System.Diagnostics;
+using System.Reactive.Disposables;
 using DynamicData.Kernel;
 
 namespace DynamicData.Cache.Internal;
@@ -15,11 +16,14 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
     private readonly ChangeAwareCache<IGroup<TObject, TKey, TGroupKey>, TGroupKey> _groupCache = new();
     private readonly Dictionary<TKey, TGroupKey> _groupKeys = [];
     private readonly HashSet<ManagedGroup<TObject, TKey, TGroupKey>> _emptyGroups = [];
+    private readonly SuspendTracker _suspendTracker = new();
     private Func<TObject, TKey, TGroupKey>? _groupSelector = groupSelector;
 
     public void AddOrUpdate(TKey key, TGroupKey groupKey, TObject item, IObserver<IGroupChangeSet<TObject, TKey, TGroupKey>>? observer = null)
     {
-        PerformAddOrUpdate(key, groupKey, item);
+        // If not emitting the changes, then suspend the notifications
+        // If changes will be emitted, then there is no need because it will generate at most one change per group
+        PerformAddOrUpdate(key, groupKey, item, observer == null ? _suspendTracker : null);
 
         if (observer != null)
         {
@@ -29,32 +33,42 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
 
     public void ProcessChangeSet(IChangeSet<TObject, TKey> changeSet, IObserver<IGroupChangeSet<TObject, TKey, TGroupKey>>? observer = null)
     {
+        var suspendTracker = (observer, changeSet.Count) switch
+        {
+            // If emitting the changeset and there is only one change, then there will be at most one change per group downstream
+            // So there's no value in suspending the notifications
+            (not null, 1) => null,
+
+            // Otherwise, use the tracker so they get suspended
+            _ => _suspendTracker,
+        };
+
         foreach (var change in changeSet.ToConcreteType())
         {
             switch (change.Reason)
             {
                 case ChangeReason.Add when _groupSelector is not null:
-                    PerformAddOrUpdate(change.Key, _groupSelector(change.Current, change.Key), change.Current);
+                    PerformAddOrUpdate(change.Key, _groupSelector(change.Current, change.Key), change.Current, suspendTracker);
                     break;
 
                 case ChangeReason.Remove:
-                    PerformRemove(change.Key);
+                    PerformRemove(change.Key, suspendTracker);
                     break;
 
                 case ChangeReason.Update when _groupSelector is not null:
-                    PerformAddOrUpdate(change.Key, _groupSelector(change.Current, change.Key), change.Current);
+                    PerformAddOrUpdate(change.Key, _groupSelector(change.Current, change.Key), change.Current, suspendTracker);
                     break;
 
                 case ChangeReason.Update:
-                    PerformUpdate(change.Key);
+                    PerformUpdate(change.Key, suspendTracker);
                     break;
 
                 case ChangeReason.Refresh when _groupSelector is not null:
-                    PerformRefresh(change.Key, _groupSelector(change.Current, change.Key), change.Current);
+                    PerformRefresh(change.Key, _groupSelector(change.Current, change.Key), change.Current, suspendTracker);
                     break;
 
                 case ChangeReason.Refresh:
-                    PerformRefresh(change.Key);
+                    PerformRefresh(change.Key, suspendTracker);
                     break;
             }
         }
@@ -75,66 +89,25 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
             return;
         }
 
-        // Create an array of tuples with data for items whose GroupKeys have changed
+        // Create tuples with data for items whose GroupKeys have changed
         var groupChanges = _groupCache.Items
-            .Select(static group => group as ManagedGroup<TObject, TKey, TGroupKey>)
-            .SelectMany(group => group!.Cache.KeyValues.Select(
-                kvp => (KeyValuePair: kvp, OldGroup: group, NewGroupKey: _groupSelector(kvp.Value, kvp.Key))))
+            .SelectMany(group => (group as ManagedGroup<TObject, TKey, TGroupKey>)!.Cache.KeyValues.Select(
+                kvp => (KeyValuePair: kvp, OldGroup: (group as ManagedGroup<TObject, TKey, TGroupKey>)!, NewGroupKey: _groupSelector(kvp.Value, kvp.Key))))
             .Where(static x => !EqualityComparer<TGroupKey>.Default.Equals(x.OldGroup.Key, x.NewGroupKey))
             .ToArray();
 
-        // Build a list of the removals that need to happen (grouped by the old key)
-        var pendingRemoves = groupChanges
-            .GroupBy(
-                static x => x.OldGroup.Key,
-                static x => (x.KeyValuePair.Key, x.OldGroup))
-            .ToDictionary(g => g.Key, g => g.AsEnumerable());
-
-        // Build a list of the adds that need to happen (grouped by the new key)
-        var pendingAddList = groupChanges
-            .GroupBy(
-                static x => x.NewGroupKey,
-                static x => x.KeyValuePair)
-            .ToList();
-
-        // Iterate the list of groups that need something added (also maybe removed)
-        foreach (var add in pendingAddList)
+        foreach (var change in groupChanges)
         {
-            // Get a list of keys to be removed from this group (if any)
-            var removeKeyList =
-                pendingRemoves.TryGetValue(add.Key, out var removes)
-                    ? removes.Select(static r => r.Key)
-                    : Enumerable.Empty<TKey>();
-
-            // Obtained the ManagedGroup instance and perform all of the pending updates at once
-            var newGroup = GetOrAddGroup(add.Key);
-            newGroup.Update(updater =>
+            PerformGroupAddOrUpdate(change.KeyValuePair.Key, change.NewGroupKey, change.KeyValuePair.Value, _suspendTracker);
+            _suspendTracker.Add(change.OldGroup);
+            change.OldGroup.Update(updater =>
             {
-                updater.RemoveKeys(removeKeyList);
-                updater.AddOrUpdate(add);
+                updater.Remove(change.KeyValuePair.Key);
+                if (updater.Count == 0)
+                {
+                    _emptyGroups.Add(change.OldGroup);
+                }
             });
-
-            // Update the key cache
-            foreach (var kvp in add)
-            {
-                _groupKeys[kvp.Key] = add.Key;
-            }
-
-            // Remove from the pendingRemove dictionary because these removes have been handled
-            pendingRemoves.Remove(add.Key);
-        }
-
-        // Everything left in the Dictionary represents a group that had items removed but no items added
-        foreach (var removeList in pendingRemoves.Values)
-        {
-            var group = removeList.First().OldGroup;
-            group.Update(updater => updater.RemoveKeys(removeList.Select(static kvp => kvp.Key)));
-
-            // If it is now empty, flag it for cleanup
-            if (group.Count == 0)
-            {
-                _emptyGroups.Add(group);
-            }
         }
 
         EmitChanges(observer);
@@ -154,6 +127,7 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
             return;
         }
 
+        // No need for Suspend Tracker.  There can't be any subscribers to the Group Caches yet so they won't be emitting any changesets.
         _groupSelector = groupSelector;
         foreach (var kvp in initialValues)
         {
@@ -183,6 +157,8 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
         // Make sure no empty ones were missed
         Debug.Assert(!_groupCache.Items.Any(static group => group.Cache.Count == 0), "Not all empty Groups were removed");
 
+        _suspendTracker.Reset();
+
         // Emit any pending changes
         var changeSet = _groupCache.CaptureChanges();
         if (changeSet.Count != 0)
@@ -191,12 +167,17 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
         }
     }
 
-    public void Dispose() => _groupCache.Items.ForEach(group => (group as ManagedGroup<TObject, TKey, TGroupKey>)?.Dispose());
+    public void Dispose()
+    {
+        _suspendTracker.Dispose();
+        _groupCache.Items.ForEach(group => (group as ManagedGroup<TObject, TKey, TGroupKey>)?.Dispose());
+    }
 
-    private static void PerformGroupRefresh(TKey key, in Optional<ManagedGroup<TObject, TKey, TGroupKey>> optionalGroup)
+    private static void PerformGroupRefresh(TKey key, in Optional<ManagedGroup<TObject, TKey, TGroupKey>> optionalGroup, SuspendTracker? suspendTracker = null)
     {
         if (optionalGroup.HasValue)
         {
+            suspendTracker?.Add(optionalGroup.Value);
             optionalGroup.Value.Update(updater => updater.Refresh(key));
         }
         else
@@ -219,7 +200,7 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
             return newGroup;
         });
 
-    private void PerformAddOrUpdate(TKey key, TGroupKey groupKey, TObject item)
+    private void PerformAddOrUpdate(TKey key, TGroupKey groupKey, TObject item, SuspendTracker? suspendTracker = null)
     {
         // See if this item already has been grouped
         if (_groupKeys.TryGetValue(key, out var currentGroupKey))
@@ -231,6 +212,7 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
                 var optionalGroup = LookupGroup(currentGroupKey);
                 if (optionalGroup.HasValue)
                 {
+                    suspendTracker?.Add(optionalGroup.Value);
                     optionalGroup.Value.Update(updater => updater.AddOrUpdate(item, key));
                     return;
                 }
@@ -240,17 +222,18 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
             else
             {
                 // GroupKey changed, so remove from old and allow to be added below
-                PerformRemove(key, currentGroupKey);
+                PerformRemove(key, currentGroupKey, suspendTracker);
             }
         }
 
         // Find the right group and add the item
-        PerformGroupAddOrUpdate(key, groupKey, item);
+        PerformGroupAddOrUpdate(key, groupKey, item, suspendTracker);
     }
 
-    private void PerformGroupAddOrUpdate(TKey key, TGroupKey groupKey, TObject item)
+    private void PerformGroupAddOrUpdate(TKey key, TGroupKey groupKey, TObject item, SuspendTracker? suspendTracker = null)
     {
         var group = GetOrAddGroup(groupKey);
+        suspendTracker?.Add(group);
         group.Update(updater => updater.AddOrUpdate(item, key));
         _groupKeys[key] = groupKey;
 
@@ -258,10 +241,10 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
         _emptyGroups.Remove(group);
     }
 
-    private void PerformRefresh(TKey key) => PerformGroupRefresh(key, LookupGroup(key));
+    private void PerformRefresh(TKey key, SuspendTracker? suspendTracker = null) => PerformGroupRefresh(key, LookupGroup(key), suspendTracker);
 
     // When the GroupKey is available, check then and move the group if it changed
-    private void PerformRefresh(TKey key, TGroupKey newGroupKey, TObject item)
+    private void PerformRefresh(TKey key, TGroupKey newGroupKey, TObject item, SuspendTracker? suspendTracker = null)
     {
         if (_groupKeys.TryGetValue(key, out var groupKey))
         {
@@ -269,13 +252,13 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
             if (EqualityComparer<TGroupKey>.Default.Equals(newGroupKey, groupKey))
             {
                 // GroupKey did not change, so just refresh the value in the group
-                PerformGroupRefresh(key, LookupGroup(groupKey));
+                PerformGroupRefresh(key, LookupGroup(groupKey), suspendTracker);
             }
             else
             {
                 // GroupKey changed, so remove from old and add to new
-                PerformRemove(key, groupKey);
-                PerformGroupAddOrUpdate(key, newGroupKey, item);
+                PerformRemove(key, groupKey, suspendTracker);
+                PerformGroupAddOrUpdate(key, newGroupKey, item, suspendTracker);
             }
         }
         else
@@ -284,11 +267,11 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
         }
     }
 
-    private void PerformRemove(TKey key)
+    private void PerformRemove(TKey key, SuspendTracker? suspendTracker = null)
     {
         if (_groupKeys.TryGetValue(key, out var groupKey))
         {
-            PerformRemove(key, groupKey);
+            PerformRemove(key, groupKey, suspendTracker);
             _groupKeys.Remove(key);
         }
         else
@@ -297,12 +280,13 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
         }
     }
 
-    private void PerformRemove(TKey key, TGroupKey groupKey)
+    private void PerformRemove(TKey key, TGroupKey groupKey, SuspendTracker? suspendTracker = null)
     {
         var optionalGroup = LookupGroup(groupKey);
         if (optionalGroup.HasValue)
         {
             var currentGroup = optionalGroup.Value;
+            suspendTracker?.Add(currentGroup);
             currentGroup.Update(updater =>
             {
                 updater.Remove(key);
@@ -320,5 +304,33 @@ internal sealed class DynamicGrouper<TObject, TKey, TGroupKey>(Func<TObject, TKe
 
     // Without the new group key, all that can be done is remove the old value
     // Consumer of the Grouper is resonsible for Adding the New Value.
-    private void PerformUpdate(TKey key) => PerformRemove(key);
+    private void PerformUpdate(TKey key, SuspendTracker? suspendTracker = null) => PerformRemove(key, suspendTracker);
+
+    private sealed class SuspendTracker : IDisposable
+    {
+        private readonly HashSet<TGroupKey> _trackedKeys = [];
+        private CompositeDisposable _disposables = [];
+
+        public bool HasItems => _disposables.Count > 0;
+
+        public void Add(ManagedGroup<TObject, TKey, TGroupKey> managedGroup)
+        {
+            if (_trackedKeys.Add(managedGroup.Key))
+            {
+                _disposables.Add(managedGroup.SuspendNotifications());
+            }
+        }
+
+        public void Reset()
+        {
+            if (_disposables.Count > 0)
+            {
+                _disposables.Dispose();
+                _disposables = [];
+                _trackedKeys.Clear();
+            }
+        }
+
+        public void Dispose() => _disposables.Dispose();
+    }
 }
