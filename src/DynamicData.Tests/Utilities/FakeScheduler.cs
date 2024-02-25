89dd4364bfa4965081703b5c6a7f1793abab8aa8
JakenVeina <jakenveina@gmail.com>
2024-02-25 02:12:40 -0600

    Fixed potential infinite-looping in the "SchedulerIsInaccurate" ExpireAfter tests.
    Reworked stress-testing for ExpireAfter operators, to better ensure that multi-threading contentions are occurring.
    
    Added test coverage for "Moved" changes, on the cache stream version of ExpireAfter.
    
    Added test coverage for "RemoveRange" changes, on the list version of ExpireAfter.

--- src/DynamicData.Tests/Utilities/FakeScheduler.cs
+++ src/DynamicData.Tests/Utilities/FakeScheduler.cs
@@ -50,6 +50,35 @@ internal sealed class FakeScheduler
             dueTime: dueTime,
             action: action);
 
+    // Simulate the scheduler invoking actions, allowing for each action to schedule followup actions, until none remain.
+    public void SimulateUntilIdle(TimeSpan inaccuracyOffset = default)
+    {
+        while(ScheduledActions.Count is not 0)
+        {
+            // If the action doesn't have a DueTime, invoke it immediately
+            if (ScheduledActions[0].DueTime is not DateTimeOffset dueTime)
+            {
+                ScheduledActions[0].Invoke();
+                ScheduledActions.RemoveAt(0);
+            }
+            // If the action has a DueTime, invoke it when that time is reached, including inaccuracy, if given.
+            // Inaccuracy is simulated by comparing each dueTime against an "effective clock" offset by the desired inaccuracy
+            // E.G. if the given inaccuracy offset is -1ms, we want to simulate the scheduler invoking actions 1ms early.
+            // For a dueTime of 10ms, that means we'd want to invoke when the clock is 9ms, so we need to the "effective clock" to be 10ms,
+            // and need to subtract the -1ms to get there.
+            else if (dueTime <= (Now - inaccuracyOffset))
+            {
+                ScheduledActions[0].Invoke();
+                ScheduledActions.RemoveAt(0);
+            }
+            // If the next action is in the future, advance time by 1ms.
+            else
+            {
+                Now += TimeSpan.FromMilliseconds(1);
+            }
+        }
+    }
+
     private IDisposable ScheduleCore<TState>(
         TState state,
         DateTimeOffset? dueTime,
