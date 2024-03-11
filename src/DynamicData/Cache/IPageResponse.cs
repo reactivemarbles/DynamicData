c63baee9bdcda03e92fcd253f2a94076e6ae4802
kronic <kronic@softland.ru>
2024-03-11 10:47:32 +0300

    Revert " ReSharper disable once CheckNamespace"
    This reverts commit 57715554bf1d1a84b7c5b7dfd8d081dcea9421e6.

--- src/DynamicData/Cache/IPageResponse.cs
+++ src/DynamicData/Cache/IPageResponse.cs
@@ -2,7 +2,6 @@
 // Roland Pheasant licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
-// ReSharper disable once CheckNamespace
 namespace DynamicData.Operators;
 
 /// <summary>
