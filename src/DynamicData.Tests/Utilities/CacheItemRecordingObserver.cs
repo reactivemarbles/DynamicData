8fd1124b08cb7abe9e30b2b29c7a53cea04336d6
Jake Meiergerd <jakenveina@gmail.com>
2024-07-21 17:59:23 -0500

    Concept/Prototype for testing utilities (#870)

---
+++ src/DynamicData.Tests/Utilities/CacheItemRecordingObserver.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Reactive.Concurrency;
+
+namespace DynamicData.Tests.Utilities;
+
+public sealed class CacheItemRecordingObserver<TObject, TKey>
+        : RecordingObserverBase<IChangeSet<TObject, TKey>>
+    where TObject : notnull
+    where TKey : notnull
+{
+    private readonly List<IChangeSet<TObject, TKey>> _recordedChangeSets;
+    private readonly Dictionary<TKey, TObject> _recordedItemsByKey;
+    private readonly List<TObject> _recordedItemsSorted;
+
+    public CacheItemRecordingObserver(IScheduler scheduler)
+        : base(scheduler)
+    {
+        _recordedChangeSets = new();
+        _recordedItemsByKey = new();
+        _recordedItemsSorted = new();
+    }        
+
+    public IReadOnlyList<IChangeSet<TObject, TKey>> RecordedChangeSets
+        => _recordedChangeSets;
+
+    public IReadOnlyDictionary<TKey, TObject> RecordedItemsByKey
+        => _recordedItemsByKey;
+
+    public IReadOnlyList<TObject> RecordedItemsSorted
+        => _recordedItemsSorted;
+
+    protected override void OnNext(IChangeSet<TObject, TKey> value)
+    {
+        if (!HasFinalized)
+        {
+            _recordedChangeSets.Add(value);
+
+            foreach (var change in value)
+            {
+                switch (change.Reason)
+                {
+                    case ChangeReason.Add:
+                        _recordedItemsByKey.Add(change.Key, change.Current);
+                        if (change.CurrentIndex is not -1)
+                            _recordedItemsSorted.Insert(change.CurrentIndex, change.Current);
+                        break;
+
+                    case ChangeReason.Moved:
+                        _recordedItemsSorted.RemoveAt(change.PreviousIndex);
+                        _recordedItemsSorted.Insert(change.CurrentIndex, change.Current);
+                        break;
+
+                    case ChangeReason.Remove:
+                        _recordedItemsByKey.Remove(change.Key);
+                        if (change.CurrentIndex is not -1)
+                            _recordedItemsSorted.RemoveAt(change.CurrentIndex);
+                        break;
+
+                    case ChangeReason.Update:
+                        _recordedItemsByKey[change.Key] = change.Current;
+                        if (change.CurrentIndex is not -1)
+                        {
+                            _recordedItemsSorted.RemoveAt(change.PreviousIndex);
+                            _recordedItemsSorted.Insert(change.CurrentIndex, change.Current);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ src/DynamicData.Tests/Utilities/RecordingObserverBase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Concurrency;
+using System.Reactive;
+
+using Microsoft.Reactive.Testing;
+
+namespace DynamicData.Tests.Utilities;
+
+// Using a custom implementing of IObserver<> to bypass normal RX safeguards, allowing invalid behaviors to be potentially tested for.
+public abstract class RecordingObserverBase<T>
+    : IObserver<T>
+{
+    private readonly List<Recorded<Notification<T>>> _notifications;
+    private readonly IScheduler _scheduler;
+
+    private Exception? _error;
+    private bool _hasCompleted;
+
+    protected RecordingObserverBase(IScheduler scheduler)
+    {
+        _notifications = new();
+        _scheduler = scheduler;
+    }
+
+    public Exception? Error
+        => _error;
+
+    public bool HasCompleted
+        => _hasCompleted;
+
+    public bool HasFinalized
+        => _hasCompleted || (_error is not null);
+
+    public IReadOnlyList<Recorded<Notification<T>>> Notifications
+        => _notifications;
+
+    protected abstract void OnNext(T value);
+
+    void IObserver<T>.OnCompleted()
+    {
+        _notifications.Add(new(
+            time: _scheduler.Now.Ticks,
+            value: Notification.CreateOnCompleted<T>()));
+
+        _hasCompleted = true;
+    }
+    
+    void IObserver<T>.OnError(Exception error)
+    {
+        _notifications.Add(new(
+            time: _scheduler.Now.Ticks,
+            value: Notification.CreateOnError<T>(error)));
+
+        if (!HasFinalized)
+            _error = error;
+    }
+
+    void IObserver<T>.OnNext(T value)
+    {
+        _notifications.Add(new(
+            time: _scheduler.Now.Ticks,
+            value: Notification.CreateOnNext(value)));
+
+        OnNext(value);
+    }
+}
