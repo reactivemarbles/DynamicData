a6c9ea1e3dff55153dc8c366cf7cff8f4700afe9
Roland Pheasant <roland_pheasant@hotmail.com>
2025-05-19 07:41:42 +0100

    Simplify Synchronize implementation

--- src/DynamicData/Internal/Rxx.cs
+++ src/DynamicData/Internal/Rxx.cs
@@ -3,54 +3,34 @@
 // See the LICENSE file in the project root for full license information.
 
 #if NET9_0_OR_GREATER
+using DynamicData.Internal;
 
 namespace System.Reactive.Linq;
 
 internal static class Rxx
 {
-    public static IObservable<T> Synchronize<T>(this IObservable<T> source, Lock locker) =>
-        new Synchronize<T>(source, locker).Run();
-}
-
-/// <summary>
-/// Keep this class internal as it should be supplied by System.Reactive and probably will be one day.
-/// </summary>
-internal sealed class Synchronize<T>
-{
-    private readonly IObservable<T> _source;
-    private readonly Lock _locker;
-
-    public Synchronize(IObservable<T> source, Lock locker)
-    {
-        _source = source;
-        _locker = locker;
-    }
-
-    public Synchronize(IObservable<T> source)
-    {
-        _source = source;
-        _locker = new Lock();
-    }
-
-    public IObservable<T> Run()
+    /// <summary>
+    /// Keep this class internal as it should be supplied by System.Reactive and probably will be one day.
+    /// </summary>
+    public static IObservable<T> Synchronize<T>(this IObservable<T> source, Lock locker)
     {
         return Observable.Create<T>(observer =>
         {
-            return _source.Subscribe(t =>
+            return source.SubscribeSafe(t =>
             {
-                lock (_locker)
+                lock (locker)
                 {
                     observer.OnNext(t);
                 }
             }, ex =>
             {
-                lock (_locker)
+                lock (locker)
                 {
                     observer.OnError(ex);
                 }
             }, () =>
             {
-                lock (_locker)
+                lock (locker)
                 {
                     observer.OnCompleted();
                 }
