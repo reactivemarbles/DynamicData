cbf4c3c81b33c7ea90549a19d018732beec8da73
Darrin W. Cullop <Darrin.Cullop@microsoft.com>
2025-05-27 13:15:57 -0700

    Support for GroupOnObservable

--- src/DynamicData/Cache/Internal/GroupOnObservable.cs
+++ src/DynamicData/Cache/Internal/GroupOnObservable.cs
@@ -13,6 +13,62 @@ internal sealed class GroupOnObservable<TObject, TKey, TGroupKey>(IObservable<IC
     where TKey : notnull
     where TGroupKey : notnull
 {
+    public IObservable<IGroupChangeSet<TObject, TKey, TGroupKey>> Run() =>
+        Observable.Create<IGroupChangeSet<TObject, TKey, TGroupKey>>(observer => new Subscription(source, selectGroup, observer));
+
+    // Maintains state for a single subscription
+    private sealed class Subscription : CacheParentSubscription<TObject, TKey, (TGroupKey, TObject), IGroupChangeSet<TObject, TKey, TGroupKey>>
+    {
+        private readonly DynamicGrouper<TObject, TKey, TGroupKey> _grouper = new();
+        private readonly Func<TObject, TKey, IObservable<TGroupKey>> _selectGroup;
+
+        public Subscription(IObservable<IChangeSet<TObject, TKey>> source, Func<TObject, TKey, IObservable<TGroupKey>> selectGroup, IObserver<IGroupChangeSet<TObject, TKey, TGroupKey>> observer)
+            : base(observer)
+        {
+            _selectGroup = selectGroup;
+            CreateParentSubscription(source);
+        }
+
+        protected override void ParentOnNext(IChangeSet<TObject, TKey> changes)
+        {
+            // Process all the changes at once to preserve the changeset order
+            foreach (var change in changes.ToConcreteType())
+            {
+                _grouper.ProcessChange(change);
+
+                switch (change.Reason)
+                {
+                    // Shutdown existing sub (if any) and create a new one that
+                    // Will update the cache and emit the changes
+                    case ChangeReason.Add or ChangeReason.Update:
+                        AddGroupSubscription(change.Current, change.Key);
+                        break;
+
+                    // Shutdown the existing subscription and remove from the cache
+                    case ChangeReason.Remove:
+                        RemoveChildSubscription(change.Key);
+                        break;
+                }
+            }
+        }
+
+        protected override void ChildOnNext((TGroupKey, TObject) tuple, TKey parentKey) =>
+            _grouper.AddOrUpdate(parentKey, tuple.Item1, tuple.Item2);
+
+        protected override void EmitChanges(IObserver<IGroupChangeSet<TObject, TKey, TGroupKey>> observer) =>
+            _grouper.EmitChanges(observer);
+
+        protected override void Dispose(bool disposing)
+        {
+            _grouper.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void AddGroupSubscription(TObject obj, TKey key) =>
+            AddChildSubscription(MakeChildObservable(_selectGroup(obj, key).DistinctUntilChanged().Select(groupKey => (groupKey, obj))), key);
+    }
+
+#if false
     public IObservable<IGroupChangeSet<TObject, TKey, TGroupKey>> Run() => Observable.Create<IGroupChangeSet<TObject, TKey, TGroupKey>>(observer =>
     {
         var grouper = new DynamicGrouper<TObject, TKey, TGroupKey>();
@@ -58,4 +114,5 @@ internal sealed class GroupOnObservable<TObject, TKey, TGroupKey>(IObservable<IC
 
         return new CompositeDisposable(shared.Connect(), subMergeMany, subChanges, grouper);
     });
+#endif
 }
