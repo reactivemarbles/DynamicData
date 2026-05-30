3de2fada7da97b86f171f7157bce3eca1adf5148
Jake Meiergerd <jakenveina@gmail.com>
2026-05-29 18:03:06 -0700

    Rewrote testing for the cache variant of the AutoRefresh() and AutoRefreshOnObservable() operators, in accordance with #1014, and in a preliminary effort to resolve #1099. (#1101)

---
+++ src/DynamicData.Tests/Cache/AutoRefreshOnObservableFixture.WithoutKey.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+using FluentAssertions;
+using Xunit;
+
+namespace DynamicData.Tests.Cache;
+
+public static partial class AutoRefreshOnObservableFixture
+{
+    public class WithoutKey
+        : Base
+    {
+        [Fact(Skip = "Existing defect: reevaluator is not null checked, throws NRW on first notification, instead")]
+        public void ReevaluatorIsNull_ThrowsException()
+            => FluentActions.Invoking(() => ObservableCacheEx.AutoRefreshOnObservable(
+                    source:         Observable.Never<IChangeSet<Item, int>>(),
+                    reevaluator:    (null as Func<Item, IObservable<Unit>>)!))
+                .Should()
+                .Throw<ArgumentNullException>();
+            
+        protected override IObservable<IChangeSet<Item, int>> BuildUut<TAny>(
+                IObservable<IChangeSet<Item, int>>  source,
+                Func<Item, IObservable<TAny>>       reevaluator,
+                TimeSpan?                           changeSetBuffer = null,
+                IScheduler?                         scheduler       = null)
+            => source.AutoRefreshOnObservable(
+                reevaluator:        reevaluator,
+                changeSetBuffer:    changeSetBuffer,
+                scheduler:          scheduler);
+    }
+}
