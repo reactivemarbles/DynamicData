b07f316292bf86295eb0c1d7e1858fd131fb5738
John Cummings <jcummings2sf@gmail.com>
2026-06-24 14:44:17 -0500

    Add RemoveKey tests showing issue with Refresh and Filter

---
+++ src/DynamicData.Tests/Cache/RemoveKeyFixture.cs
@@ -0,0 +1,123 @@
+#region
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Disposables;
+
+using DynamicData.Binding;
+using DynamicData.Tests.Domain;
+
+using FluentAssertions;
+
+using Xunit;
+
+#endregion
+
+namespace DynamicData.Tests.Cache;
+
+public class RemoveKeyFixture : IDisposable
+{
+    private readonly IComparer<Person> _comparer;
+
+    private readonly RandomPersonGenerator _generator = new();
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2213:Disposable fields should be disposed", Justification = "Handled with CompositeDisposable")]
+    private readonly ISourceCache<Person, string> _source;
+
+    private readonly CompositeDisposable _cleanup = new();
+
+    public RemoveKeyFixture()
+    {
+        _comparer = SortExpressionComparer<Person>.Ascending(p => p.Age).ThenByAscending(p => p.Name);
+
+        _source = new SourceCache<Person, string>(p => p.Key);
+        _cleanup.Add(_source);
+    }
+
+    public void Dispose() => _cleanup.Dispose();
+
+    [Fact]
+    public void Add()
+    {
+        ReadOnlyObservableCollection<Person> collection;
+        _cleanup.Add(
+            _source.Connect()
+                .RemoveKey()
+                .Bind(out collection)
+                .Subscribe()
+        );
+        var people = _generator.Take(100).ToArray();
+        _source.AddOrUpdate(people);
+
+        Assert.Equivalent(people, collection);
+    }
+
+    [Fact]
+    public void Filter()
+    {
+        var people = _generator.Take(100).ToArray();
+        var average = people.Average(x => x.Age);
+
+        ReadOnlyObservableCollection<Person> collection;
+        _cleanup.Add(
+            _source.Connect()
+                .RemoveKey()
+                .Filter(x => x.Age < average)
+                .Bind(out collection)
+                .Subscribe()
+        );
+        _source.AddOrUpdate(people);
+
+        Assert.Equivalent(people.Where(x => x.Age < average), collection);
+    }
+
+    [Fact]
+    public void Refresh()
+    {
+        ReadOnlyObservableCollection<Person> collection;
+        _cleanup.Add(
+            _source.Connect()
+                .AutoRefresh(x => x.Age)
+                .RemoveKey()
+                .Bind(out collection)
+                .Subscribe()
+        );
+        var people = _generator.Take(100).ToArray();
+        _source.AddOrUpdate(people);
+
+        Assert.Equivalent(people, collection);
+
+        foreach (var person in people)
+        {
+            person.Age = person.Age + 1;
+        }
+        Assert.Equivalent(people, collection);
+    }
+
+    [Fact]
+    public void RefreshFilter()
+    {
+        var people = _generator.Take(100).ToArray();
+        var average = people.Average(x => x.Age);
+        ReadOnlyObservableCollection<Person> collection;
+        _cleanup.Add(
+            _source.Connect()
+                .AutoRefresh(x => x.Age)
+                .RemoveKey()
+                .Filter(x => x.Age < average)
+                .Bind(out collection)
+                .Subscribe()
+        );
+        _source.AddOrUpdate(people);
+
+        Assert.Equivalent(people.Where(x => x.Age < average), collection);
+
+        foreach (var person in people)
+        {
+            person.Age = person.Age + 1;
+        }
+        Assert.Equivalent(people.Where(x => x.Age < average), collection);
+    }
+}
